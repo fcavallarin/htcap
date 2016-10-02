1b2a4eaf41e1490bb2cc45db961334b51d001952
segment-srl <filippo.cavallarin@segment.technology>
2016-10-02 16:02:52 +0200

    added login utility

---
+++ core/util/utilities/login.py
@@ -0,0 +1,129 @@
+# -*- coding: utf-8 -*- 
+
+"""
+HTCAP - htcap.org
+Author: filippo.cavallarin@wearesegment.com
+
+This program is free software; you can redistribute it and/or modify it under
+the terms of the GNU General Public License as published by the Free Software
+Foundation; either version 2 of the License, or (at your option) any later
+version.
+"""
+
+import sys
+import sqlite3
+import json
+import getopt 
+import os
+import getpass
+
+from core.lib.utils import *
+from core.lib.shell import CommandExecutor
+from core.util.base_util import BaseUtil
+from core.lib.cookie import Cookie
+
+reload(sys)
+sys.setdefaultencoding('utf8')
+
+class Login(BaseUtil):
+
+	@staticmethod
+	def get_settings():
+		return dict(
+			descr = "Login to a webapp to get session cookies and logout urls",
+			optargs = 'p:HJhAcl',
+			minargs = 2
+		)
+
+	def usage(self):
+		return (
+			"%s\n"
+			"usage: %s [options] <url> <username> [<text_of_submit_element>]\n"
+			"Options:\n"
+			"   -h            This help\n"
+			"   -p PASSWD     Set login password\n"
+			"   -c            Do not output cookies\n"
+			"   -l            Do not output logout urls\n"
+			"   -H            Format output as htcap arguments\n"
+			"   -J            Format output as json (for cookies only)\n"
+			"   -A            Format output as general command line arguments\n"
+			% (self.get_settings()['descr'], self.utilname)
+		)
+
+
+	def main(self, args, opts):
+		passw = None
+		format = None
+		out_cookies = True
+		out_logouts = True
+		for o,v in opts:
+			if o == "-h":
+				print self.usage()
+				sys.exit(0)
+			elif o == "-p":
+				passw = v
+			elif o == "-c":
+				out_cookies = False
+			elif o == "-l":
+				out_logouts = False
+			elif o in  ("-H", "-J", "-A"):
+				format = o
+
+		if not passw:
+			print "The password is hidden here BUT it will be passed to phantomjs via commandline ..."
+			try:
+				passw = getpass.getpass()
+			except KeyboardInterrupt:
+				print "\nAbort..."
+				sys.exit(0)
+
+		jspath = "%s%s%s%s" % (getrealdir(__file__), "login", os.sep, "login.js")
+		cmd = get_phantomjs_cmd() + [jspath, args[0], args[1], passw]
+		if len(args) > 2: cmd.append(args[2])
+		#print cmd_to_str(cmd)
+		exe = CommandExecutor(cmd, True)
+		out, err = exe.execute(20)
+		if err:
+			print "Unable to login"
+			sys.exit(1)
+
+		try:
+			ret = json.loads(out)
+		except ValueError as e:
+			print e
+			sys.exit(1)
+		allcookies, logouts = ret
+		cookies = []
+		if out_cookies:
+			for c in reversed(allcookies):
+				cookie = Cookie(c)
+				if not cookie in cookies: cookies.append(cookie)
+		if not out_logouts:
+			logouts = []
+
+		if not format:
+			for c in cookies:
+				print "%s=%s" % (c.name, c.value)
+			for u in logouts:
+				print u
+		elif format == "-A":
+			for c in cookies:
+				print cmd_to_str([c.name, c.value])
+			for u in logouts:
+				print cmd_to_str([u])
+		elif format == "-H":
+			args = []
+			if len(cookies) > 0:
+				args = ["-c", ";".join(["%s=%s" % (c.name, c.value) for c in cookies])]
+			if len(logouts) > 0:
+				args.extend(["-x", ",".join(logouts)])
+			if len(args) > 0:
+				print cmd_to_str(args)
+		elif format == "-J":
+			cd = []
+			for c in cookies:
+				cd.append(c.get_dict())
+			if out_cookies:
+				print json.dumps(cd)
+
+
---
+++ core/util/utilities/login/login.js
@@ -0,0 +1,213 @@
+/*
+HTCAP - htcap.org
+Author: filippo.cavallarin@wearesegment.com
+
+This program is free software; you can redistribute it and/or modify it under
+the terms of the GNU General Public License as published by the Free Software
+Foundation; either version 2 of the License, or (at your option) any later
+version.
+*/
+
+var system = require('system');
+var fs = require('fs');
+
+// ctrl-c from probe/function.js .. maybe I should create some common.js and put it somewhere .. but where?.. really.. where??
+function getCookies(headers, url){
+	var a, b, c, ret = [];
+	var purl = document.createElement('a');
+	purl.href = url;
+	var domain = purl.hostname;
+
+	for(a = 0; a < headers.length; a++){
+		//console.log(JSON.stringify(headers[a]))
+		if(headers[a].name.toLowerCase() == "set-cookie"){
+			var cookies = headers[a].value.split("\n");	 // phantomjs stores multiple cookies in this way ..
+			for(b = 0; b < cookies.length; b++){
+				var ck = cookies[b].split(/; */);
+				var cookie = {domain: domain, path: "/", secure: false, httponly:false};
+				for(c = 0; c < ck.length; c++){
+					var kv = ck[c].split("=");
+					if(c == 0){
+						cookie.name = kv[0];
+						cookie.value = decodeURIComponent(kv[1]);
+						continue;
+					}
+					switch(kv[0].toLowerCase()){
+						case "expires":
+							if(!("expires" in cookie))
+								cookie.expires = parseInt((new Date(kv[1])).getTime() / 1000);
+							break;
+						case "max-age":
+							cookie.expires = parseInt(((new Date()).getTime() / 1000) + parseInt(kv[1]));
+							break;
+						case "domain":
+						case "path":
+							cookie[kv[0]] = kv[1];
+							break;
+						case "httponly":
+						case "secure":
+							cookie[kv[0]] = true;
+							break;
+					}
+				}
+				ret.push(cookie);
+			}
+		}
+	}
+	return ret;
+};
+
+
+
+function print(mess){
+	output.push(mess)
+}
+
+if(system.args.length < 4){
+	console.log("usage: login.js <url> <login> <password> [<button_text>]");
+	phantom.exit(0);
+}
+
+
+var step = 0;
+var allCookies = [];
+var output = []
+
+var url = system.args[1];
+var login = system.args[2];
+var password = system.args[3];
+var buttonTxt = system.args[4] || null;
+
+
+var page = require('webpage').create();
+
+page.onConsoleMessage = function(msg, lineNum, sourceId) {
+
+	//console.log("console: " + msg);
+}
+
+page.onResourceReceived = function(response) {
+		var cookies = getCookies(response.headers, url)
+		for(var a = 0; a < cookies.length; a++){
+			allCookies.push(cookies[a])
+		}
+};
+
+phantom.onError = function(msg, trace) {}
+page.onError = function(msg, trace) {}
+
+page.onCallback = function(data) {
+	switch(data.cmd){
+		case "next":
+			step = 2;
+			//page.render("010101.png")
+			return;
+		case "end":
+			console.log(JSON.stringify(output))
+			phantom.exit(0);
+		case "print":
+			print(JSON.parse(data.par))
+			return;
+	}
+}
+
+page.onLoadFinished = function(status) {
+	if(status != 'success'){
+		console.log("error loading page " + url);
+		phantom.exit(1);
+	}
+
+	if(step < 2)return;
+
+	if(allCookies.length > 0){
+		print(allCookies);
+	} else {
+		print([]);
+	}
+
+	page.evaluate(function(){
+		var els = document.getElementsByTagName("a");
+		var lu = [];
+		var re = /.*(log|sign)(_|\-| )?(out|off).*/gi;
+		for(var a = 0; a < els.length; a++){
+			if(els[a].href.match(re) || els[a].innerText.match(re)){
+				lu.push(els[a].href)
+			}
+		}
+		if(lu.length > 0){
+			callPhantom({cmd:"print",par:JSON.stringify(lu)});
+		} else {
+			callPhantom({cmd:"print",par:'[]'});
+		}
+
+		callPhantom({cmd:"end"});
+	})
+}
+
+
+
+page.settings.loadImages = false;
+
+page.open(url, {}, function(status){
+	page.evaluate(function(login, password,buttonTxt){
+		function trigger(el, evname){
+			if ('createEvent' in document) {
+				var evt = document.createEvent('HTMLEvents');
+				evt.initEvent(evname, true, false);
+				el.dispatchEvent(evt);
+			} else {
+				evname = 'on' + evname;
+				if( evname in el && typeof el[evname] == "function"){
+					el[evname]();
+				}
+			}
+		};
+
+		function getAdiacent(cont, selector){
+			var ad = null;
+
+			while(!ad && cont){
+				ad = cont.querySelector(selector)
+				cont = cont.parentNode
+			}
+			return ad
+		}
+
+		var passw_el = document.querySelector("input[type=password]");
+		passw_el.value = password;
+		var login_el = getAdiacent(passw_el, "input[type=text],input[type=email],input:not([type])");
+		var button_el = null;
+		if(buttonTxt){
+			var els = document.getElementsByTagName("*");
+			for(var a = 0; a < els.length; a++){
+				for(var ch = els[a].firstChild; ch; ch = ch.nextSibling){
+					if(ch.nodeType != 3)continue; // skip non textNodes
+					if(ch.nodeValue.toLowerCase().trim() == buttonTxt.toLowerCase()){
+						button_el = els[a];
+						break;
+					}
+				}
+			}
+		} else {
+			button_el = getAdiacent(passw_el, "input[type=submit],button");
+			if(!button_el){
+				button_el = getAdiacent(passw_el, "a");
+			}
+		}
+		if(!login_el || ! button_el){
+			console.log("error")
+		}
+		login_el.value = login;
+		trigger(login_el, "blur")
+		trigger(login_el, "change")
+		trigger(passw_el, "blur")
+		trigger(passw_el, "change")
+
+		setTimeout(function(){
+			trigger(button_el, "click");
+			callPhantom({cmd:"next"});
+		},50);
+
+	},login, password, buttonTxt);
+});
+
