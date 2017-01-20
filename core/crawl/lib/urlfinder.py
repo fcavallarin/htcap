29fc876db0db568a2ed932fb674776c15a48e0b5
Guillaume Grasset <guillaume@delvelabs.ca>
2017-01-20 16:36:10 -0500

    Add test on UrlFinder and implement basic <base> tag support

--- core/crawl/lib/urlfinder.py
+++ core/crawl/lib/urlfinder.py
@@ -15,24 +15,36 @@ from HTMLParser import HTMLParser
 
 
 class UrlFinder:
-	def __init__(self, html):
-		self.html = html
+    def __init__(self, html):
+        self.html = html
 
-	def get_urls(self):
-		urls = []
+    def get_urls(self):
+        urls = []
 
-		class UrlHTMLParser(HTMLParser):			
-			def handle_starttag(self, tag, attrs):
-				if tag == "a":
-					#urls.extend([attr[1] for attr in attrs if attr[0] == "href" and attr[1] != ""])
-					#urls.extend([val for key,val in attrs if key == "href" and val != ""])
-					urls.extend([val for key,val in attrs if key == "href" and (re.match("^https?://", val, re.I) or (not re.match("^[a-z]+:", val, re.I) and not val.startswith("#")))])
-			
-		try:
-			parser = UrlHTMLParser()
-			parser.feed(self.html)
-		except:
-			raise
+        class UrlHTMLParser(HTMLParser):
+            def __init__(self):
+                HTMLParser.__init__(self)
+                self.base_tag_href = ""
 
-		
-		return urls
+            def handle_starttag(self, tag, attrs):
+                # more info about the <base> tag: https://www.w3.org/wiki/HTML/Elements/base
+                if tag == "base":
+                    for key, val in attrs:
+                        if key == "href" and re.match("^https?://", val, re.I):
+                            self.base_tag_href = val.strip()
+
+                elif tag == "a":
+                    for key, val in attrs:
+                        if key == "href":
+                            if re.match("^https?://", val, re.I):
+                                urls.extend([val])
+                            elif not re.match("^[a-z]+:", val, re.I) and not val.startswith("#"):
+                                urls.extend(['{}{}'.format(self.base_tag_href, val)])
+
+        try:
+            parser = UrlHTMLParser()
+            parser.feed(self.html)
+        except:
+            raise
+
+        return urls
